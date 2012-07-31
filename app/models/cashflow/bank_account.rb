0e896a759ff0946d4f817aefa235e40ba0c9d0b9
Rob Taylor <Robert_taylor_@hotmail.com>
2012-07-31 23:48:59 +0100

    refactored daily balances

--- app/models/cashflow/bank_account.rb
+++ app/models/cashflow/bank_account.rb
@@ -10,21 +10,23 @@ module Cashflow
     def withdraw(ammount, date)
       transactions.create!(:credit => ammount, :date => date)
     end
+    
     # return the balance at the start of a given date, default to tomorrow to to get up to date balance
     def balance(date=Date.tomorrow)
       transactions.before_date(date).sum(:debit) - transactions.before_date(date).sum(:credit)
     end
+    
     # return the bank activity for the specified date
     def activity(date)
       transactions.for_date(date).sum(:debit) - transactions.for_date(date).sum(:credit)
     end
+    
     # return daily balances for a date range...
     def daily_balances(start_date, end_date)
       balance = balance(start_date)
       balances = Array.new
-      balances << {:date => start_date, :balance => balance }
-      ((start_date+1)..(end_date)).each do |date|
-        balance += activity(date-1) 
+      ((start_date)..(end_date)).each do |date|
+        balance += activity(date-1) unless date == start_date
         balances << {:date => date, :balance => balance }
       end 
       balances
