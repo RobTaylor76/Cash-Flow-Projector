e1c4a122618a0359c98bef3aca158db35eb0e30c
Rob Taylor <Robert_taylor_@hotmail.com>
2012-08-01 21:39:03 +0100

    removed trailing white space on blank lines

--- app/models/cashflow/bank_account.rb
+++ app/models/cashflow/bank_account.rb
@@ -1,4 +1,4 @@
-module Cashflow 
+module Cashflow
   class BankAccount < ActiveRecord::Base
 
     has_many :transactions
@@ -10,17 +10,17 @@ module Cashflow
     def withdraw(ammount, date)
       transactions.create!(:credit => ammount, :date => date)
     end
-    
+
     # return the balance at the start of a given date, default to tomorrow to to get up to date balance
     def balance(date=Date.tomorrow)
       transactions.before_date(date).sum(:debit) - transactions.before_date(date).sum(:credit)
     end
-    
+
     # return the bank activity for the specified date
     def activity(date)
       transactions.for_date(date).sum(:debit) - transactions.for_date(date).sum(:credit)
     end
-    
+
     # return daily balances for a date range...
     def daily_balances(start_date, end_date)
       balance = balance(start_date)
