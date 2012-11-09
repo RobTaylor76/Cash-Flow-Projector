cc4f7f11ec7198f85fb5554b91b1cb3ae694f760
Rob Taylor <robert_taylor_@hotmail.com>
2012-11-09 22:44:33 +0000

    reworked cache lookup data to cope with table not existing

--- app/models/cashflow/transaction_frequency.rb
+++ app/models/cashflow/transaction_frequency.rb
@@ -1,9 +1,14 @@
 class Cashflow::TransactionFrequency < ActiveRecord::Base
    attr_accessible :name
 
-   self.all.each do  |freq|
-     define_singleton_method "#{freq.name.downcase}" do
-         freq
+   def self.cache_lookup_data
+     if self.table_exists?
+       self.all.each do  |freq|
+         define_singleton_method "#{freq.name.downcase}" do
+           freq
+         end
+       end
      end
    end
+   cache_lookup_data
 end
