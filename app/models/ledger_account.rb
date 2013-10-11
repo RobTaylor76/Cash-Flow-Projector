b55b4a0b8d520950d15c591fadd422c02264c95b
Rob Taylor <robert.taylor2@sage.com>
2013-10-11 17:17:52 +0100

    reworked daily balances to use sql to agregate the daily balances. couldnt use scope.to_sql as there was wierd prepared statement stuff going on

--- app/models/ledger_account.rb
+++ app/models/ledger_account.rb
@@ -34,12 +34,41 @@ class LedgerAccount < ActiveRecord::Base
   # return daily balances for a date range...
   def daily_balances(start_date, end_date)
     balance = balance(start_date)
+
+    data = execute_sql(daily_balances_sql(start_date, end_date))
+
     balances = Array.new
     ((start_date)..(end_date)).each do |date|
-      delta = activity(date)
+      delta = if data[date].present?
+                   delta = data[date]['activity'].to_d
+                 else
+                   0.00
+                 end
       balances << {:date => date, :balance => balance, :activity => delta }
       balance += delta
     end
     balances
+  end
+
+  private 
+
+  def daily_balances_sql(start_date, end_date)
+    sql = "select date, sum(debit) - sum(credit) as activity "
+    sql +="from ledger_entries where ledger_account_id = #{self.id} "
+    sql +="and (date >= Date('#{start_date}') AND date <= Date('#{end_date}')) "
+    sql +="group by date"
+  end
+  def execute_sql(sql)
+    results = ActiveRecord::Base.connection.select_all(sql)
+    transform_data_to_hash(results)
+  end
+
+  def transform_data_to_hash(data_set)
+    hash = {}
+    data_set.each do |item|
+      key = Date.parse(item['date'])
+      hash[key] = item
+    end
+    hash
   end
 end
