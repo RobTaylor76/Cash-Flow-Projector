63e34edefc493fba3a99679d080684dda570f077
Rob Taylor <Robert_taylor_@hotmail.com>
2013-11-03 21:41:39 +0000

    storing filter dates in session

--- app/controllers/concerns/date_range_filterable.rb
+++ app/controllers/concerns/date_range_filterable.rb
@@ -5,25 +5,37 @@ module DateRangeFilterable
     scope.date_range_filter(@date_range_filter.start_date,@date_range_filter.end_date)
   end
 
+  def store_in_session(key, value)
+    session[key] = value
+  end
+
+  def get_from_session(key, default)
+    session[key] ||= default
+    session[key]
+  end
+
   def set_up_date_range_filter(filter_path)
     date_range_params =  params[:date_range_filter] ||= {}
 
     if date_range_params[:start_date].present?
       date_range_params[:start_date] = Date.parse(date_range_params[:start_date])
+      store_in_session(:start_date, date_range_params[:start_date])
     else
-      date_range_params[:start_date] = Date.today.beginning_of_month
+      date_range_params[:start_date] = get_from_session(:start_date, Date.today.beginning_of_month)
     end
 
     if date_range_params[:end_date].present?
       date_range_params[:end_date] = Date.parse(date_range_params[:end_date])
+      store_in_session(:end_date, date_range_params[:end_date])
     else
-      date_range_params[:end_date] = Date.today.end_of_month
+      date_range_params[:end_date] = get_from_session(:end_date, Date.today.end_of_month)
     end
 
     if date_range_params[:balance_date].present?
       date_range_params[:balance_date] = Date.parse(date_range_params[:balance_date])
+      store_in_session(:balance_date, date_range_params[:end_date])
     else
-      date_range_params[:balance_date] = Date.today
+      date_range_params[:balance_date] = get_from_session(:balance_date, Date.today)
     end
 
     if date_range_params[:balance_date] > date_range_params[:end_date]
