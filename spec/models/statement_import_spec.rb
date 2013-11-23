ca2650494872d31d3c8130d27438203d3adfdc51
Rob Taylor <Robert_taylor_@hotmail.com>
2013-11-23 23:34:44 +0000

    fixed build

--- spec/models/statement_import_spec.rb
+++ spec/models/statement_import_spec.rb
@@ -1,8 +1,20 @@
 require 'spec_helper'
 
+
 describe StatementImport do
-  it { subject.should have_many(:transactions) }
-  it { subject.should belong_to(:user) }
-  it { subject.should belong_to(:ledger_account) }
+
+  before :each do
+    @user = User.find_by_email('test_user@cashflowprojector.com')
+    @la = @user.ledger_accounts.last
+    @statement_import = @la.statement_imports.build
+  end
+  describe :relationships do
+
+    it :has_em do
+      @statement_import.should have_many(:transactions)
+      @statement_import.should belong_to(:user)
+      @statement_import.should belong_to(:ledger_account)
+    end
+  end
 
 end
