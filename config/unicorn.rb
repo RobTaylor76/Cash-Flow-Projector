9b773665287b1546b6fe9e5d1288abb1725135e0
Rob Taylor <rtaylor@clixifix.co.uk>
2017-12-08 14:56:38 +0000

    tweaked unicorn config to be same as clixifix

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,7 +1,20 @@
-# config/unicorn.rb
-worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
+# need :tcp_nopush => false 	# for streaming
+# see https://gist.github.com/nathany/5058140 for more info
+# nb backlog has a minimum size of 16 iirc
+listen ENV['PORT'], :backlog => Integer(ENV['UNICORN_BACKLOG'] || 32), :tcp_nopush => false
+
+worker_processes (ENV["unicorn_workers"] || 3).to_i     # number of unicorn workers to spin up
+
+timeout (ENV["unicorn_timeout"] || 60).to_i         	# restarts workers that don't return after X seconds
+
 timeout 30
+
+# combine Ruby 2.0.0dev or REE with "preload_app true" for memory savings
+# http://rubyenterpriseedition.com/faq.html#adapt_apps_for_cow
 preload_app true
+GC.respond_to?(:copy_on_write_friendly=) and
+    GC.copy_on_write_friendly = true
+
 
 before_fork do |server, worker|
   Signal.trap 'TERM' do
@@ -18,6 +31,14 @@ after_fork do |server, worker|
     puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
   end
 
-  defined?(ActiveRecord::Base) and
-      ActiveRecord::Base.establish_connection
+  # the following is *required* for Rails + "preload_app true",
+  # more details added from https://devcenter.heroku.com/articles/concurrency-and-database-connections
+  if defined?(ActiveRecord::Base)
+    config = Rails.application.config.database_configuration[Rails.env]
+    config['reaping_frequency'] = ENV['DB_REAP_FREQ'] || 10 # seconds
+    config[:reaping_frequency]  = ENV['DB_REAP_FREQ'] || 10 # seconds
+    config['pool']              = ENV['DB_POOL'] || 5
+    config[:pool]               = ENV['DB_POOL'] || 5
+    info = ActiveRecord::Base.establish_connection(config)
+  end
 end
